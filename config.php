d896667b72884c08b2f9ac5541807c1e5c6b710d
Pavitra Pai <111533080+pavitrapai10@users.noreply.github.com>
2024-08-07 14:04:09 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'vdsms');
+
+$conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
+if ($conn == false){
+    die('ERROR: Could not connect. '.mysqli_connect_error());
+}
+else {
+    echo "Connected successfully";
+}
+?>
---
+++ header.php
@@ -0,0 +1,93 @@
+ <?php
+ include("config.php")
+ ?>
+ <!-- header section starts -->
+ <header class="header_section">
+    <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg custom_nav-container pt-3">
+        <a class="navbar-brand" href="index.html">
+          <img src="img/logo.jpeg" alt="logo" />
+          <span>
+            Vogue Design School Management
+          </span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto mr-2">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">About </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="service.php">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="portfolio.php">Portfolio </a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="contact.php">Contact us</a>
+            </li>
+          </ul>
+          <div class="user_option">
+            <div class="login_btn-container">
+              <a href="">
+                Login
+              </a>
+            </div>
+            <form class="form-inline my-2 my-lg-0">
+              <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
+            </form>
+          </div>
+        </div>
+        <div class="call_btn">
+          <a href="">
+            Call: +01234567890
+          </a>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <!-- end header section -->
+  <!-- custom menu -->
+  <div class="custom_menu-container">
+    <div class="container">
+      <div class="custom_menu">
+        <ul class="navbar-nav ">
+          <li class="nav-item active">
+            <a class="nav-link pl-0" href="index.php">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">About </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="service.php">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="portfolio.php">Portfolio </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact us</a>
+          </li>
+        </ul>
+        <div class="user_option">
+          <div class="login_btn-container">
+            <a href="">
+              Login
+            </a>
+          </div>
+          <form class="form-inline my-2 my-lg-0">
+            <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- custom menu -->
